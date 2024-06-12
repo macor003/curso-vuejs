9a146827e5af70967206ca06a0772c7dcd289d2c
Mario Ortega <mcor63@gmail.com>
2024-06-12 17:11:32 -0500

    Se agrega proyecto

---
+++ 01-bases/src/main.ts
@@ -0,0 +1,16 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+import './bases/08-promises.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+  </div>
+`
